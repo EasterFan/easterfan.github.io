3477dc86963e39f97e1104c94c32747e5f78aba3
EasterFan <easterfan@gmail.com>
2023-05-19 11:12:47 +0800

    Auto-update

--- blogo.sh
+++ blogo.sh
@@ -5,5 +5,5 @@ cd /Users/$USER/workspace/common_repo/easterfan.github.io
 TODAY=`date +%Y-%m-%d`
 ORIGIN=`ls _posts | grep -v '^[0-9]' | head -n 1`
 
-mv $ORIGIN $TODAY-$ORIGIN
+mv "_posts/$ORIGIN" "_posts/$TODAY-$ORIGIN"
 git add . && git commit -m 'Auto-update' && git pull origin master --rebase && git push origin master
