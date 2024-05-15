d8f1272ba1e9fe7243b19573b1d42b9cb439219b
easterfan <easterfan@163.com>
2024-05-15 11:58:34 +0800

    Auto-update

--- blogo.sh
+++ blogo.sh
@@ -1,9 +1,11 @@
 #bin/sh
 
 cd /Users/$USER/workspace/common_repo/easterfan.github.io 
+mv _posts/300_Resource/blog/* _posts/
 
-TODAY=`date +%Y-%m-%d`
-ORIGIN=`ls _posts | grep -v '^[0-9]' | head -n 1`
 
-mv "_posts/$ORIGIN" "_posts/$TODAY-$ORIGIN"
+# TODAY=`date +%Y-%m-%d`
+# ORIGIN=`ls _posts | grep -v '^[0-9]' | head -n 1`
+
+# mv "_posts/$ORIGIN" "_posts/$TODAY-$ORIGIN"
 git add . && git commit -m 'Auto-update' && git pull origin master --rebase && git push origin master
